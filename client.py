5568363bdd48cead898c85585afa0af68b19bb9d
yanir <yanircohen75@gmail.com>
2021-11-29 17:33:36 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,23 @@
+import time
+from socket import *
+import imgui
+target_port = 10000  # create a socket object
+client = socket(AF_INET, SOCK_STREAM)
+host = gethostname()
+# connect the client
+client.connect((host, target_port))
+
+# send some data
+request = f'GET /helloworld.html'
+client.send(request.encode())
+
+# receive some data
+try:
+    time.sleep(2)
+    response = client.recv(4096).decode()
+    print(response)
+except:
+    print("error")
+
+# display the response
+
---
+++ server.py
@@ -0,0 +1,103 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+from threading import Thread
+
+
+def soc(port):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    host = gethostname()
+    print(port)
+    server_address = (host, port)
+    port += 1
+    serverSocket.bind(server_address)
+    serverSocket.listen(1)
+
+    while True:
+        print('Ready to serve...')
+
+        connectionSocket, addr = serverSocket.accept()
+        thread = Thread(target=soc, args=(port,))
+        thread.start()
+
+        try:
+
+            message = connectionSocket.recv(1024).decode()
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = 'HTTP/1.0 200 OK\n'
+            outputdata += 'Content-Type: text/html\n\n'
+            outputdata += f.read()
+            f.close()
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            # print("message sent")
+            connectionSocket.close()
+            sys.exit()
+        except IOError:
+            outputdata = 'HTTP/1.0 404 Not Found\n'
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+            sys.exit()
+
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# get the hostname of myserver easier for us to reach and copy
+host = gethostname()
+# print the host so we would know what it is
+print(host)
+port = 10000
+# create a vector with the hostname and port (random port we chose)
+server_address = (host, port)
+
+# binding the socket to the specific address
+serverSocket.bind(server_address)
+# listen to only 1 connection at a time.
+serverSocket.listen(1)
+# Prepare a sever socket
+# Fill in start
+# Fill in end
+li = []
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+
+    # establishing the connection
+
+    connectionSocket, addr = serverSocket.accept()
+    # print("connection established")
+    port += 1
+    thread = Thread(target=soc, args=(port,))
+    thread.start()
+    try:
+        # receive the message
+        message = connectionSocket.recv(1024).decode()
+        # your code (gets the request from the message and open a file accordingly then remove the / from the start and opens if the file exists)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        # creating the response according to the message header and html file
+        outputdata = 'HTTP/1.0 200 OK\n'
+        outputdata += 'Content-Type: text/html\n\n'
+        outputdata += f.read()
+        # closing the file
+        f.close()
+        # Fill in start       #Fill in end
+        # Send one HTTP header line into socket
+        # Fill in start
+        # Fill in end
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        # print("message sent")
+        connectionSocket.close()
+    except IOError:
+        # returns not found in case of an error as requested
+        outputdata = 'HTTP/1.0 404 Not Found\n'
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
